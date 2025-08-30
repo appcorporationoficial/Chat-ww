45a1b66f8457d56e7f99748ff387217635274991
AppCorporation <104796009+appcorporationoficial@users.noreply.github.com>
2025-08-29 21:40:09 -0600

    Actualizar pchat.js

--- pchat.js
+++ pchat.js
@@ -55,7 +55,7 @@ app.get("/ultimos-mensajes", (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Servidor en http://localhost:${PORT}`);
 });
